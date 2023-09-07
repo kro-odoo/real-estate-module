e59cfbdd74a9a7af0064c04f9653b0d7a36ab8a2
Krina Oza <kro@odoo.com>
2023-09-07 19:52:39 +0530

    [ADD] estate: added pdf report

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -10,6 +10,7 @@
         'views/estate_property_views.xml',
         'views/estate_property_tags_views.xml',
         'views/estate_menus.xml',
+        'reports/estate_property_report.xml',
     ],
     'demo': [],
     'application':True,
